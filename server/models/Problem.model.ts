07aebdf3650dfde89617853540e26669b47a6905
Pulkit Gupta <pulkitgupta95700@gmail.com>
2025-04-13 18:19:20 +0530

    add problem handler made

--- server/models/Problem.model.ts
+++ server/models/Problem.model.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface IProblem extends Document {
@@ -12,6 +13,18 @@ export interface IProblem extends Document {
   url: string;
   is_rev: boolean;
   difficulty: "Easy" | "Medium" | "Hard" | "Unknown";
+  tags: {
+    tag_id: mongoose.Schema.Types.ObjectId; // FK reference to Tag
+    tag_name: string;
+  }[]; // FK reference to Tag
+  inventories: {
+    inventory_id: mongoose.Schema.Types.ObjectId; // FK reference to Inventory
+    inventory_name: string;
+  }[]; // FK reference to Inventory
+  companies: {
+    company_id: mongoose.Schema.Types.ObjectId; // FK reference to Company
+    company_name: string;
+  }[]; // FK reference to
 }
 
 // Problem Schema
@@ -36,9 +49,48 @@ const ProblemSchema = new Schema<IProblem>(
       enum: ["Easy", "Medium", "Hard", "Unknown"],
       default: "Unknown",
     },
+    tags: [
+      {
+        tag_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Tag",
+        },
+        tag_name: { type: String },
+      },
+    ],
+    inventories: [
+      {
+        inventory_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Inventory",
+        },
+        inventory_name: { type: String },
+      },
+    ],
+    companies: [
+      {
+        company_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Company",
+        },
+        company_name: { type: String },
+      },
+    ],
   },
   { timestamps: true }
 );
+
+ProblemSchema.index({ user_id: 1, inventories: 1 });
+ProblemSchema.index({ user_id: 1, "tags.tag_id": 1 });
+ProblemSchema.index({ user_id: 1, "companies.company_id": 1 });
+ProblemSchema.index({ user_id: 1, problem_id: 1 }, { unique: true });
+ProblemSchema.index({ user_id: 1, difficulty: 1 });
+ProblemSchema.index({ user_id: 1, platform_name: 1 });
+ProblemSchema.index({ user_id: 1, problem_title: 1 });
+ProblemSchema.index({ problem_title: "text" });
+ProblemSchema.index({ user_id: 1, is_rev: 1 });
+
+// deletng the problem will erase its tagslookups and inventorylookups and companylookups
 
 export default mongoose.models.Problem ||
   mongoose.model<IProblem>("Problem", ProblemSchema);
